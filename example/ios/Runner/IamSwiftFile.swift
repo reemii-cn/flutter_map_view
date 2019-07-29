d56df234db7232cabd86cbb3fe3822a32f7916e1
arther <wangzhuang@reemii.cn>
2019-07-29 17:09:32 +0800

    配置swift版本

---
+++ example/ios/Runner/IamSwiftFile.swift
@@ -0,0 +1,13 @@
+//
+//  IamSwiftFile.swift
+//  Runner
+//
+//  Created by arther on 2019/7/29.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import UIKit
+
+class IamSwiftFile: NSObject {
+
+}
